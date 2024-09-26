698b28ce05fb515eada62e88c1eec44c3449e6d3
nguyenduy574 <nguyenduy574@outlook.com>
2024-09-26 12:56:10 +0700

    ssss

--- my_pos/settings.py
+++ my_pos/settings.py
@@ -84,10 +84,6 @@ WSGI_APPLICATION = 'my_pos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    """ 'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } """
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/my_pos',
