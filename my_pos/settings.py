ea5a2d0037cabf614dd0f40889a55967f3661f60
nguyenduy574 <nguyenduy574@outlook.com>
2024-09-26 12:51:08 +0700

    d

--- my_pos/settings.py
+++ my_pos/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,10 +84,15 @@ WSGI_APPLICATION = 'my_pos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    """ 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    } """
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/my_pos',
+        conn_max_age=600
+    )
 }
 
 
