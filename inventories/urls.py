77eef2ca4bff1233b9a7051f6c1f1bc14f6ca36e
nguyenduy574 <nguyenduy574@outlook.com>
2024-09-27 16:29:51 +0700

    fix url

--- inventories/urls.py
+++ inventories/urls.py
@@ -5,5 +5,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 app_name = "inventories"
 urlpatterns = [
     path("", views.inventories, name="inventories"),
-    path("inventories/detail/<slug:slug>", views.detail, name="detail"),
+    path("detail/<slug:slug>", views.detail, name="detail"),
 ] + debug_toolbar_urls()
